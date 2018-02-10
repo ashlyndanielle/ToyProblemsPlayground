d06ce23f5243bc21afee07729e33122807b01dec
Ashlyn Mitros <ashlyndmitros@gmail.com>
2018-02-09 21:36:55 -0700

    first attempt at EvensAndOdds, completed

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
 
 class EvenAndOdd extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userInput: '',
+      evens: [],
+      odds: []
+    }
+  }
+
+  sortArray = input => {
+    const parsedArray = [];
+    const evens = [];
+    const odds = [];
+    input.split(',').forEach( num => {
+      parsedArray.push(parseInt(num, 10));
+    });
+    parsedArray.forEach( num => {
+      num%2 === 0 ? evens.push(num + ', ') : odds.push(num + ', ');
+    })
+    this.setState({
+      userInput: '',
+      evens,
+      odds
+    })
+  }
+
+  handleInput = e => {
+    this.setState({
+      userInput: e.target.value
+    })
+  }
+
   render() {
     return (
-      <div>
+      <div className="puzzleBox evenAndOddPB">
         <h4>Even and Odd</h4>
+        <input
+          className="inputLine"
+          onChange={ this.handleInput }
+          value={ this.state.input }
+        />
+        <button className="confirmationButton" onClick={ input => this.sortArray(this.state.userInput) }>Sort Array</button>
+        <span className="resultsBox">Evens: { this.state.evens }</span>
+        <span className="resultsBox">Odds: { this.state.odds }</span>
       </div>
     );
   }
