6a46cd33b0468dae3010b219e2262e57e669019a
Ashlyn Mitros <ashlyndmitros@gmail.com>
2018-02-14 11:32:21 -0700

    finished filter string component

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 
 class FilterString extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: '',
+      unfilteredStrings: ['happy birthday', 'merry christmas', 'happy thanksgiving', 'happy holidays', 'balloons', 'party', 'cake', 'thank you', 'jiminy christmas'],
+      filteredStrings: []
+    }
+  }
+
+  handleInput = e => {
+    this.setState({
+      userInput: e.target.value
+    })
+  }
+
+  filterStrings = text => {
+    const strings = this.state.unfilteredStrings;
+    const filtered = [];
+    for ( let x = 0; x < strings.length; x++ ) {
+      strings[x].includes(text) ?
+        filtered.push(strings[x]) :
+        console.log('does not include')
+    }
+    this.setState({
+      filteredStrings: filtered,
+      userInput: ''
+    })
+  }
+
   render() {
     return (
-      <div>
+      <div className="puzzleBox filterStringPB">
         <h4>FilterString</h4>
+        <span className="puzzleText">{ JSON.stringify(this.state.unfilteredStrings, null, 10) }</span>
+        <input className="inputLine" onChange={ this.handleInput } value={ this.state.userInput }/>
+        <button className="confirmationButton" onClick={ () => this.filterStrings(this.state.userInput) }>Filter</button>
+        <span className="resultsBox filterStringRB">{ JSON.stringify(this.state.filteredStrings, null, 10) }</span>
       </div>
     );
   }
